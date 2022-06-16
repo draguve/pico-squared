1f9884864df5dcb73cdc70c3286d3cb5e638344d
Draguve <ritwikpatil1998@gmail.com>
2022-06-17 01:40:13 +0530

    Building our visitor

--- main.cpp
+++ main.cpp
@@ -13,26 +13,15 @@
 #include <iostream>
 
 #include "antlr4-runtime.h"
-#include "TLexer.h"
-#include "TParser.h"
+#include "LuaVisitor.h"
 
-using namespace antlrcpptest;
 using namespace antlr4;
+using namespace LuaLanguage;
 
 int main(int , const char **) {
   ANTLRInputStream input(u8"🍴 = 🍐 + \"😎\";(((x * π))) * µ + ∰; a + (x * (y ? 0 : 1) + z);");
-  TLexer lexer(&input);
-  CommonTokenStream tokens(&lexer);
 
-  tokens.fill();
-  for (auto token : tokens.getTokens()) {
-    std::cout << token->toString() << std::endl;
-  }
-
-  TParser parser(&tokens);
-  tree::ParseTree* tree = parser.main();
-
-  std::cout << tree->toStringTree(&parser) << std::endl << std::endl;
+  std::cout << "Test";
 
   return 0;
 }
